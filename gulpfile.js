1f60c48d1ce3ca500567819105565c14fdb7da85
Eduardo Mota <eduardo_motaknc@hotmail.com>
2017-09-15 06:45:38 -0300

    fim da atividade

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var del = require("del");
 var htmlmin = require('gulp-html-minifier');
 var sass = require('gulp-sass');
+const notify 	= require("gulp-notify");
  
 gulp.task("clean-html", function(){
 	del("./dist/index.html");
@@ -11,24 +12,33 @@ gulp.task("clean-sass", function(){
 	del("./dist/css/");
 })
 
-gulp.task('html-minify',['clean-html'], function() {
+gulp.task('html', function() {
  	return gulp.src('./source/index.html')
-    .pipe(htmlmin({collapseWhitespace: true}))
+    //.pipe(htmlmin({collapseWhitespace: true}))
     .pipe(gulp.dest('./dist/'))
 });
 
-gulp.task('sass-minify',['clean-sass'], function () {
- return gulp.src('./source/scss/*.scss')
-   .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-   .pipe(gulp.dest('./dist/css'));
+gulp.task('sass', function () {
+	 return gulp.src('./source/style.scss')
+ 		.pipe(sass())
+ 		.on("error", notify.onError({title:"erro ao compilar", message:"<%= error.message %>"}))
+   //.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+		.pipe(gulp.dest('./dist/css'))
 });
 
-gulp.task('html-background', function(){
-	gulp.watch('./source/index.html',['html-minify']);
+
+gulp.task('html:watch', function(){
+	gulp.watch('./source/index.html',['html']);
+
 });
 
-gulp.task('sass-background', function(){
-	gulp.watch('./source/scss/*.scss',['sass-minify']);
+gulp.task("sass:watch", function(){
+	gulp.watch("./source/scss/*.scss", ['sass']);
+	gulp.watch("./source/style.scss", ['sass']);
 });
+
+gulp.task("default",['sass','html', 'sass:watch','html:watch']);
+
+
 
 
